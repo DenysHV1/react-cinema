d1e87dc8fca1cc21d3f016a3854866e19f8423e8
DenVik <denyagarkusha2@gmail.com>
2024-12-20 16:38:48 +0100

    add effect

--- src/components/FilmsList/FilmsList.jsx
+++ src/components/FilmsList/FilmsList.jsx
@@ -1,17 +1,18 @@
+import { useState } from "react";
 import s from "./FilmsList.module.css";
 
-//hooks
+// hooks
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router";
 
-//redux
+// redux
 import { imgLink } from "../../redux/helpSettings";
 import {
   isErrorSelector,
   isLoadingSelector,
 } from "../../redux/filmsPage/selectors";
 
-//components
+// components
 import Error from "../Error/Error";
 import Loader from "../Loader/Loader";
 import { FcLike } from "react-icons/fc";
@@ -23,6 +24,27 @@ const FilmsList = ({ films = [] }) => {
   const isLoading = useSelector(isLoadingSelector);
   const location = useLocation();
   const dispatch = useDispatch();
+
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [hoverTitle, setHoverTitle] = useState("");
+
+  const handleMouseMove = (e, title) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+    setHoverTitle(title);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverTitle("");
+  };
+
+  const cursorStyle = {
+    position: "fixed",
+    top: `${cursorPosition.y - 20}px`,
+    left: `${cursorPosition.x}px`,
+    pointerEvents: "none",
+    transform: "translate(-50%, -50%)",
+    zIndex: 9999,
+  };
 
   return (
     <section onClick={() => dispatch(closeMenu())}>
@@ -36,7 +58,12 @@ const FilmsList = ({ films = [] }) => {
               id &&
               vote_average > 3 &&
               popularity > 1 && (
-                <li key={id} className={s.list_element}>
+                <li
+                  key={id}
+                  className={s.list_element}
+                  onMouseMove={(e) => handleMouseMove(e, title)}
+                  onMouseLeave={handleMouseLeave}
+                >
                   <Link to={`/films/${id}`} state={location}>
                     <img src={`${imgLink}${poster_path}`} alt={title} />
                     <div className={s.popularity_container}>
@@ -57,6 +84,12 @@ const FilmsList = ({ films = [] }) => {
           </li>
         )}
       </ul>
+
+      {hoverTitle && (
+        <div className={s.custom_cursor} style={cursorStyle}>
+          {hoverTitle}
+        </div>
+      )}
     </section>
   );
 };
