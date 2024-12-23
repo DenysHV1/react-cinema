c03042c980c68caff8c3368a732dd7482ecf93ed
DenVik <denyagarkusha2@gmail.com>
2024-12-23 14:46:00 +0100

    fix continue watching

--- src/components/LastVideos/LastVideos.jsx
+++ src/components/LastVideos/LastVideos.jsx
@@ -12,61 +12,61 @@ import { Navigation, Mousewheel, Keyboard } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import NoInfo from "../NoInfo/NoInfo";
 import { selectLastVideos } from "../../redux/lastVideo/lastVideoReducer";
 
 const LastVideos = () => {
   const lastVideos = useSelector(selectLastVideos);
 
   console.log(lastVideos);
-  
+
   return (
     <>
-	<h2 className={s.title}>Continue watching</h2>
-      {lastVideos?.length > 0 ? (
-        <Swiper
-          cssMode={true}
-          navigation={true}
-          mousewheel={true}
-          keyboard={true}
-          modules={[Navigation, Mousewheel, Keyboard]}
-          breakpoints={{
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 15,
-            },
-            1024: {
-              slidesPerView: 4,
-              spaceBetween: 20,
-            },
-            1440: {
-              slidesPerView: 4,
-              spaceBetween: 20,
-            },
-          }}
-        >
-          {lastVideos.map(
-            ({ id, key, name }, idx) =>
-              key && idx < 11 &&(
-                <SwiperSlide key={id} className={s.videoContainer}>
-                  <iframe
-                    className={s.iframeVideo}
-                    src={`https://www.youtube.com/embed/${key}`}
-                    title={name}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
-                  ></iframe>
-                </SwiperSlide>
-              )
-          )}
-        </Swiper>
-      ) : (
-        <NoInfo info="changes" />
+      {lastVideos.length > 0 && (
+        <>
+          <h2 className={s.title}>Continue watching</h2>
+          <Swiper
+            cssMode={true}
+            navigation={true}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Mousewheel, Keyboard]}
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 15,
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+              },
+              1440: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+              },
+            }}
+          >
+            {lastVideos.map(
+              ({ id, key, name }, idx) =>
+                key &&
+                idx < 11 && (
+                  <SwiperSlide key={id} className={s.videoContainer}>
+                    <iframe
+                      className={s.iframeVideo}
+                      src={`https://www.youtube.com/embed/${key}`}
+                      title={name}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowFullScreen
+                    ></iframe>
+                  </SwiperSlide>
+                )
+            )}
+          </Swiper>
+        </>
       )}
     </>
   );
